487cd274e59097ea4af157e34f70dd37b88fdc9b
kjh-sw <dkk2617@kookmin.ac.kr>
2020-06-26 15:45:55 +0900

    issue #k1 #2 add calculate

---
+++ fig.py
@@ -0,0 +1,23 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response) :
+    d = parse_qs(environ['QUERY_STRING'])
+    sum = 0
+    mul = 0
+    x = d.get('x',[''])[0]
+    y = d.get('y',[''])[0]
+    if '' not in [x,y] :
+	x,y = int(x), int(y)
+	sum = [x+y]
+	mul = [x*y]
+    response_body = html%{
+	    'sum' : sum,
+	    'mul' : mul
+	}
+    
+    start_response('200 OK', [
+        ('Content-Type', 'text/html'),
+	('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
---
+++ template.py
@@ -0,0 +1,15 @@
+html=b"""
+<html>
+    <body>
+        <form method="get" action="">
+	    x=<input type="number" name="x">
+	    y=<input type="number" name="y">
+	    <input type="submit">
+        </form>
+        <p>
+	    sum:%(sum)x</br>
+	    mul:%(mul)s</br>
+	</p>
+    </body>
+</html>
+"""
